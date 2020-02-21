4451bf464708327a7fafde2e66aa1671a40cdfd8
Laura Louise Cabral e Silva <louise.laura@academico.ifrn.edu.br>
2020-02-21 15:32:39 -0300

    exercicio 1

---
+++ Lista1/Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite as notas dos dois bimestres: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int m = (a * 2 + b * 3) / 5;
+            Console.WriteLine($"Média parcial = {m}");
+        }
+    }
+}
